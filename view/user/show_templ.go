b53097040c3d9ef16903ae93b11910d26e43b53b
Igor Wulff <i.wulff@youweagency.com>
2024-05-24 14:52:55 +0200

    Moved layout to components and restructured.

--- view/user/show_templ.go
+++ view/user/show_templ.go
@@ -12,11 +12,11 @@ import "bytes"
 
 import (
 	"youwe.com/go-web-accelerator/components"
+	layout "youwe.com/go-web-accelerator/components/layout"
 	"youwe.com/go-web-accelerator/models"
-	"youwe.com/go-web-accelerator/view/layout"
 )
 
-func Show(user model.User) templ.Component {
+func Show(user models.User) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
